dd43755aa704aec4f2566366a82c9918d36a6332
Herbert Henrique <herbertmatos10@hotmail.com>
2022-11-29 21:52:53 -0300

    refactor function component to class component, implement handler functions and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLinkClick = this.handleLinkClick.bind(this);
+    this.state = {
+      name: 'Herbert',
+      counter: 0
+    };
+  }
+
+  handleTextClick() {
+    this.setState({name: 'Junior'});
+  }
+
+  // if you use arrow, you don't need bind the function
+  handleLinkClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    const nextCounter = counter + 1;
+    this.setState({ counter: nextCounter });
+  }
+
+  render() {
+    const { name, counter } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick ={ this.handleTextClick }>
+           { name } - { counter } 
+          </p>
+          <a
+            onClick = { this.handleLinkClick }
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
