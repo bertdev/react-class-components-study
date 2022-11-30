f56a4f8d96fef4fd98ceba042f95cfae1d27105a
Herbert Henrique <herbertmatos10@hotmail.com>
2022-11-29 22:16:36 -0300

    remove handler functions and use map to render some posts

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleLinkClick = this.handleLinkClick.bind(this);
     this.state = {
-      name: 'Herbert',
-      counter: 0
+      posts: [
+        {
+          id: 1,
+          title: 'Post 1',
+          content: 'Content 1'
+        },
+        {
+          id: 2,
+          title: 'Post 2',
+          content: 'Content 1'
+        }
+      ]
     };
   }
 
-  handleTextClick() {
-    this.setState({name: 'Junior'});
-  }
-
-  // if you use arrow, you don't need bind the function
-  handleLinkClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    const nextCounter = counter + 1;
-    this.setState({ counter: nextCounter });
-  }
-
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick ={ this.handleTextClick }>
-           { name } - { counter } 
-          </p>
-          <a
-            onClick = { this.handleLinkClick }
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          posts.map(post => {
+            return (
+              <div key={post.id}> 
+                <h1>{post.title}</h1>
+                <p>{post.content}</p>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
